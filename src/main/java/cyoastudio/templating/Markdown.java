f49165f5b74fd2e9c6477543c2138ecdf0141e90
Quantencomputer <>
2017-09-24 16:44:11 +0200

    Actually implement markdown extensions

--- src/main/java/cyoastudio/templating/Markdown.java
+++ src/main/java/cyoastudio/templating/Markdown.java
@@ -1,12 +1,25 @@
 package cyoastudio.templating;
 
+import java.util.Arrays;
+
 import com.vladsch.flexmark.ast.Document;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.ext.typographic.TypographicExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.superscript.SuperscriptExtension;
+import com.vladsch.flexmark.util.options.*;
 
 public class Markdown {
-	private static Parser parser = Parser.builder().build();
-	private static HtmlRenderer renderer = HtmlRenderer.builder().build();
+	private static final MutableDataHolder options = new MutableDataSet()
+			.set(Parser.EXTENSIONS, Arrays.asList(
+					TablesExtension.create(),
+					StrikethroughExtension.create(),
+					TypographicExtension.create(),
+					SuperscriptExtension.create()));
+	private static final Parser parser = Parser.builder(options).build();
+	private static final HtmlRenderer renderer = HtmlRenderer.builder(options).build();
 
 	public static String render(String source) {
 		Document document = parser.parse(source);
